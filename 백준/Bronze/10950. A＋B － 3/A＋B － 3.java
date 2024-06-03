3deebe8d2375605191f84ad2775b24e0ae19bc6a
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-03 11:41:55 +0900

    [Bronze V] Title: A+B - 3, Time: 224 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int A,B ;
+		
+		for(int i = 0; i < n; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println(A + B);
+		}
+	}
+}
