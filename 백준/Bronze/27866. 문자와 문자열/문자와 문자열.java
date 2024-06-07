8fb5ddd627085acba0c8a6a4f7c09e30d398fd3e
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-07 17:10:44 +0900

    [Bronze V] Title: 문자와 문자열, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+
+        System.out.println(s.charAt(n-1));
+
+
+    }
+}
