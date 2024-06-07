cef0e262e3fe17673ff87be7f01808fd9f8d56d5
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-08 00:29:12 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,40 +1,44 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = 0;
         String st = br.readLine();
-        int len = st.length();
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < st.length(); i++) {
             char ch = st.charAt(i);
-            if (ch == 'c' && i < len - 1) {
+            if (ch == 'c' && i < st.length() - 1) {
                 if (st.charAt(i + 1) == '=' || st.charAt(i + 1) == '-') {
                     i++;
                 }
-            } else if (ch == 'd' && i < len - 1) {
-                if (i < len - 2 && st.charAt(i + 1) == 'z' && st.charAt(i + 2) == '=') {
+            } else if (ch == 'd' && i < st.length() - 1) {
+                if (i < st.length() - 2 && st.charAt(i + 1) == 'z' && st.charAt(i + 2) == '=') {
                     i += 2;
                 } else if (st.charAt(i + 1) == '-') {
                     i++;
                 }
-            } else if (ch == 'l' && i < len - 1) {
+            } else if (ch == 'l' && i < st.length() - 1) {
                 if (st.charAt(i + 1) == 'j') {
                     i++;
                 }
-            } else if (ch == 'n' && i < len - 1) {
+
+            } else if (ch == 'n' && i < st.length() - 1) {
                 if (st.charAt(i + 1) == 'j') {
                     i++;
                 }
-            } else if (ch == 's' && i < len - 1) {
+
+            } else if (ch == 's' && i < st.length() - 1) {
                 if (st.charAt(i + 1) == '=') {
                     i++;
                 }
-            } else if (ch == 'z' && i < len - 1) {
+
+            } else if (ch == 'z' && i < st.length() - 1) {
                 if (st.charAt(i + 1) == '=') {
                     i++;
                 }
+
             }
             count++;
         }
