2fe03b824a5c20cb65e28cb40c2c2dcb15b4c1da
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-11 12:26:44 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+class Main {
+    public static void main(String[] args){
+
+        System.out.println(1);
+        System.out.println(0);
+        
+    }
+}
