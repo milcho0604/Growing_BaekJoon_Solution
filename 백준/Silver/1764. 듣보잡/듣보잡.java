bbb0ab481f3c8de2dec1230c3770424a063ea65e
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-19 14:32:42 +0900

    [Silver IV] Title: 듣보잡, Time: 256 ms, Memory: 26268 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,65 @@
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//
+//        String[] sp = br.readLine().split(" ");
+//        int n = Integer.parseInt(sp[0]);
+//        int m = Integer.parseInt(sp[1]);
+//
+//        Set<String> mySet = new HashSet<>();
+//        for(int i=0; i<n; i++){
+//            mySet.add(br.readLine());
+//        }
+//        StringBuilder sb = new StringBuilder();
+//        int count =0;
+//        for(int i=0; i<m; i++){
+//            String str = br.readLine();
+//            if(mySet.contains(str)){
+//                sb.append(str).append("\n");
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+//        System.out.println(sb);
+//    }
+//}
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String[] sp = br.readLine().split(" ");
+        int n = Integer.parseInt(sp[0]);
+        int m = Integer.parseInt(sp[1]);
+        Set<String> mySet = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            mySet.add(br.readLine());
+        }
+        int count = 0;
+        List<String> myList = new ArrayList<>();
+        for (int i = 0; i < mySet.size(); i++) {
+            String str = br.readLine();
+            if (mySet.contains(str)) {
+                myList.add(str);
+                count++;
+            }
+        }
+        myList.sort(Comparator.naturalOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < myList.size(); i++) {
+            sb.append(myList.get(i)).append("\n");
+        }
+        System.out.println(count);
+        System.out.println(sb);
+    }
+
+}
