90eee6ec87b0bf0a3fcd16e06a3318f9ad6d7076
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-19 14:35:06 +0900

    [Silver IV] Title: 듣보잡, Time: 256 ms, Memory: 26168 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,33 +1,3 @@
-//import java.io.*;
-//import java.util.*;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st;
-//
-//        String[] sp = br.readLine().split(" ");
-//        int n = Integer.parseInt(sp[0]);
-//        int m = Integer.parseInt(sp[1]);
-//
-//        Set<String> mySet = new HashSet<>();
-//        for(int i=0; i<n; i++){
-//            mySet.add(br.readLine());
-//        }
-//        StringBuilder sb = new StringBuilder();
-//        int count =0;
-//        for(int i=0; i<m; i++){
-//            String str = br.readLine();
-//            if(mySet.contains(str)){
-//                sb.append(str).append("\n");
-//                count++;
-//            }
-//        }
-//        System.out.println(count);
-//        System.out.println(sb);
-//    }
-//}
-
 import java.io.*;
 import java.util.*;
 
