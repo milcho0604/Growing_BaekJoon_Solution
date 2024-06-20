03cf6ec7ba2e8ac90e0faf1ab951d7b03d6c44ff
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-20 19:15:22 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 664 ms, Memory: 227040 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -12,6 +12,7 @@ public class Main {
         for (int i = 0; i < str.length(); i++) {
             for (int j = i+1; j <= str.length(); j++) {
                 mySet.add(str.substring(i, j));
+                // 예를 들어, i=0이고 j=1이라면 substring(0,1),(0,2),(0,3),(0,4),...(1,2),... 이와 같은 방법으로 반복
             }
         }
         System.out.println(mySet.size());
