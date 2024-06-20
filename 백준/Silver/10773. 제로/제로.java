c74e81fa0f755156b4d6987312c6410ab0e66ea5
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-20 17:39:00 +0900

    [Silver IV] Title: 제로, Time: 252 ms, Memory: 30788 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,3 +1,26 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.*;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//
+//        int n = Integer.parseInt(br.readLine());
+//
+//        Deque<Integer> de = new ArrayDeque<>();
+//
+//
+//        for(int i=0; i<n; i++){
+//
+//        }
+//
+//    }
+//}
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,18 +34,20 @@ public class Main {
 
         Stack<Integer> numStack = new Stack<>();
         int n = Integer.parseInt(br.readLine());
-        int result = 0;
+
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int in = Integer.parseInt(st.nextToken());
             if (in != 0) {
-                result += numStack.push(in);
+                numStack.push(in);
             } else {
-                result -= numStack.pop();
+                numStack.pop();
             }
         }
 
+        int result = numStack.stream().reduce(0,(a, b) -> a + b);
         System.out.println(result);
+
     }
 }
