ab4c394abc0795df137580dc9f9bf34ba4833d63
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-22 19:26:29 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45436 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Deque<Integer> cardQue = new LinkedList<>();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        cardQue(n);
+    }
+
+    public static void cardQue(int n) {
+        for (int i = 1; i <= n ; i++) {
+            cardQue.add(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(cardQue.size()>1) {
+                cardQue.pollFirst();
+                cardQue.addLast(cardQue.pollFirst());
+            }
+        }
+        sb.append(cardQue.pollFirst());
+
+        System.out.println(sb);
+    }
+
+}
