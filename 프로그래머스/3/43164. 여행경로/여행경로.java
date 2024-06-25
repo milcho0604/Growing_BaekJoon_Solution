248bfd0c8cbaecdcb72704fb0971e5b7bf715895
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-25 21:38:53 +0900

    [level 3] Title: 여행경로, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,40 +1,44 @@
 import java.util.*;
-
 class Solution {
-    static boolean[] visited;
-    static List<String> myList;
-    static List<String> path;
-    static String[][] tickets;
-
+    static boolean visited[];
+    static List<String> myList = new ArrayList<>();
+    static Stack<String> temp;
+    static String[] answer;
     public String[] solution(String[][] tickets) {
-        Solution.tickets = tickets;
+        answer = new String[tickets.length];
+        temp = new Stack<>();
         visited = new boolean[tickets.length];
-        myList = new ArrayList<>();
-        path = new ArrayList<>();
-
-        // 시작점 "ICN"을 경로에 추가
-        path.add("ICN");
-
-        dfs(0, "ICN");
-
-        // 가능한 경로 중 사전순으로 가장 앞서는 경로 선택
+        int line = 0;
+        temp.add("ICN");
+        dfs(line, "ICN", tickets);
         Collections.sort(myList);
-        return myList.get(0).split(",");
+//        System.out.println(myList);
+//        answer = myList;
+        // System.out.println(myList);
+        answer = myList.toArray(new String[0]);
+        // System.out.println(Arrays.deepToString(answer));
+        return answer;
     }
 
-    private void dfs(int line, String now) {
+    private static void dfs(int line, String now, String[][] tickets) {
         if (line == tickets.length) {
-            myList.add(String.join(",", path));
+//            myList.add(Arrays.toString(temp));
+//            myList.add(String.valueOf(temp));
+            myList.add(String.join(",", temp));
+//            answer = temp.toArray(new String[0]);
             return;
         }
         for (int i = 0; i < tickets.length; i++) {
             if (!visited[i] && tickets[i][0].equals(now)) {
                 visited[i] = true;
-                path.add(tickets[i][1]);
-                dfs(line + 1, tickets[i][1]);
+//                temp[i]= tickets[i][1];
+                temp.add(tickets[i][1]);
+                dfs(line + 1, tickets[i][1], tickets);
                 visited[i] = false;
-                path.remove(path.size() - 1);
+                temp.remove(temp.size()-1);
             }
         }
     }
+    
+    
 }
