e1274fbadc8607e6e66c5da8f6e21653654ae03e
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-06-29 23:35:56 +0900

    [Bronze II] Title: 소수, Time: 172 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static boolean prime[];
+	
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int M = in.nextInt();
+		int N = in.nextInt();
+		
+		prime = new boolean[N + 1];
+		get_prime();
+		
+		
+		// 소수 합 및 최솟값 
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i = M; i <= N; i++) {
+			if(prime[i] == false) {	// false = 소수 
+				sum += i;
+				if(min == Integer.MAX_VALUE) {	// 첫 소수가 최솟값
+					min = i;
+				}
+			}
+		}
+		
+		if(sum == 0) {	// 소수가 없다면 
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+		
+	}
+ 
+	public static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+		
+	}
+}
