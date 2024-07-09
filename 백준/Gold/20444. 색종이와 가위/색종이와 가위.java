a9d60343fa698b201712fcfd5dc0e94a247b0c68
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-07-09 22:08:56 +0900

    [Gold V] Title: 색종이와 가위, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/20444. 색종이와 가위/색종이와 가위.java
+++ 백준/Gold/20444. 색종이와 가위/색종이와 가위.java
@@ -27,7 +27,7 @@ public class Main {
         while (left <= right) {
             long mid = (left + right) / 2;  // 중간값 계산(자르는 지점의 인덱스)
             long value = n - mid;           // 자를 횟수에서 중간값을 뺀 값(자르고 난 후 남은 부분의 길이)
-
+            
 //            목표값 계산(개수 계산이라는 것을 위해 +1을 함)
 //            아래 target은 구글링을 통해 얻은 계산
             long target = (mid + 1) * (value + 1);
@@ -41,6 +41,6 @@ public class Main {
                 left = mid + 1;  // 목표값이 작으면 왼쪽에 +1
             }
         }
-        sb.append("NO");  // 찾지 못하면 "NO" 출력
+        sb.append("NO");
     }
 }
