8a4fd3817a84edcd05de40a53f73228cf42a73e7
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-12-08 16:57:00 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 30572 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+//        String[] spl = br.readLine().split(" ");
+        int n = Integer.parseInt(br.readLine()); // 테스트 케이스(도시)
+
+        int[] distance = new int[n - 1]; // 도시마다 거리
+        int[] price = new int[n]; // 각 도시 주유소의 가격
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            distance[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        int minPrice = price[0];
+        for (int i = 0; i < n-1; i++) {
+            if (price[i] < minPrice) {
+                minPrice = price[i];
+            }
+            sum += minPrice * distance[i];
+        }
+        System.out.println(sum);
+    }
+}
