805fbcf6800153e16cf8717ff9e978de0dfbba66
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-12-21 23:48:49 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 45676 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -7,6 +7,7 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+        
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         cardQue(n);
