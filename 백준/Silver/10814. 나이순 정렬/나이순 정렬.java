c76c06db86237ac14df0b16feb8f70e18c04f1f1
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-12-27 15:40:04 +0900

    [Silver V] Title: 나이순 정렬, Time: 772 ms, Memory: 53876 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,38 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+
+    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+//        String[] spl = br.readLine().split(" ");
+        int n = Integer.parseInt(br.readLine());
         String arr[][] = new String[n][3];
-        for (int i = 0; i < n; i++) {
-            arr[i][0]= sc.next();
-            arr[i][1]= sc.next();
-            arr[i][2]= String.valueOf(i);
-        }
 
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+            arr[i][2] = String.valueOf(i);
+        }
         Arrays.sort(arr, new Comparator<String[]>() {
             @Override
             public int compare(String[] o1, String[] o2) {
                 int age = Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0]));
-                if (age != 0) {
+                if (age != 0){
                     return age;
-                } else {
+                }else {
                     return Integer.compare(Integer.parseInt(o1[2]), Integer.parseInt(o2[2]));
                 }
             }
         });
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(" ")
+                    .append(arr[i][1]).append(" ").append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
