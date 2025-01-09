855b11ce66836802e7418da839d12bdb6be16315
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2025-01-09 23:45:41 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -34,13 +34,13 @@ public class Main {
     }
 
     private static void dfs(int x, int y, int[][] maps) {
-//        종료 조건 : x 또는 y가 map 범위를 벗어나거나, visited가 이미 방문한 위치라면, answer=true(이비 목적지에 도착했다면)
+//      종료 조건 : x 또는 y가 map 범위를 벗어나거나, visited가 이미 방문한 위치라면, answer=true(이비 목적지에 도착했다면)
         if (x >= n || y >= n || visited[x][y] || answer) return;
         if (maps[x][y] == -1) {
             answer = true;
             return;
         }
-//        방문한 배열 위치, 방문처리
+//      방문한 배열 위치, 방문처리
         visited[x][y] = true;
 
         for (int i = 0; i < 2; i++) {
