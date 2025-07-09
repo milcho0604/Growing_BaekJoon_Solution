8de0801c5ce8a4e4432d88c4a4e10f7c7969698a
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2025-07-09 23:24:08 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,17 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
+public class Main {
+
+    static StringTokenizer st;
+//    static int arr[][] = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int arr[] = new int [n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i+1;
         }
-        for (int i = 0; i < M; i++) {
-            int a = sc.nextInt() - 1;
-            int b = sc.nextInt() - 1;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+
             while(a<b){
                 int temp = arr[a];
                 arr[a] = arr[b];
@@ -20,8 +32,11 @@ class Main {
                 b--;
             }
         }
-        for(int i=0; i< arr.length; i++){
-            System.out.print(arr[i]+ " ");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i] + " ");
         }
+        System.out.println(sb.toString());
+
     }
+
 }
