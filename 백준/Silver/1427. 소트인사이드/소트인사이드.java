c77ac306670902024b7976525e2a71dde35a8ad3
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2025-07-25 01:06:23 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,25 +1,30 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+
+    static StringTokenizer st;
+//    static int arr[][] = new int[30][30];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int n= Integer.parseInt(br.readLine());
-        String str = String.format(String.valueOf(n));
-        String arr[] = new String[str.length()];
+        st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<str.length(); i++){
-            arr[i] = String.valueOf(str.charAt(i));
+        int n = Integer.parseInt(st.nextToken());
+        String st = String.format(String.valueOf(n));
+        String arr [] = new String[st.length()];
+
+        for (int i = 0; i < st.length(); i++) {
+            arr[i] = String.valueOf(st.charAt(i));
         }
         Arrays.sort(arr, Collections.reverseOrder());
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
         }
-
-
+        System.out.println(sb.toString());
     }
 }
