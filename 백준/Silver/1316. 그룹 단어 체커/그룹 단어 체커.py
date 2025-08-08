97e7e89195ebea55d1730dfbdb4e03f3f98d0bfe
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2025-08-08 15:44:46 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,10 +1,10 @@
-def is_group_word(word):
+def search(word):
     visited = set()
     before = ""
-
+    
     for char in word:
-        if char != before:
-            if char not in visited:
+        if(char != before):
+            if(char not in visited):
                 visited.add(char)
                 before = char
             else:
@@ -16,7 +16,7 @@ count = 0
 
 for _ in range(n):
     word = input()
-    if is_group_word(word):
-        count += 1
-
+    if search(word):
+        count +=1
+        
 print(count)
