f67924f0172c49ca78f570b0fbb27395f8ffbfe7
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2025-08-10 20:30:03 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1316 ms, Memory: 78268 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,11 +1,20 @@
 import sys
-# n = int(input())
 
 input = sys.stdin.readline
-n = int(input().strip())
-arr = [int(input()) for _ in range(n)]
+n = int(input())
 
-arr.sort()
+result = []
 
-for i in range(len(arr)):
-    print(arr[i])
+for _ in range(n):
+    result.append(int(input()))
+
+result.sort()
+
+for i in result:
+    print(i)
+
+# for i in range(len(result)):
+    # print(result[i])
+    
+    # 만약 한 줄로 1 2 3 4 5와 같이 출력하고 싶다면 아래 end 추가
+    # print(result[i], end=' ')
