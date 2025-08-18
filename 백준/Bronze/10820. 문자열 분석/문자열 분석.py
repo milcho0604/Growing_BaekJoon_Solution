36195e230f83ec709fe2849fbe787b80dc10575d
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2025-08-18 14:15:35 +0900

    [Bronze II] Title: 문자열 분석, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,19 +1,18 @@
 import sys
-# for문에서 input()과 같은 역할로 입력을 받음
-# sys.stdin은 
+
 for line in sys.stdin:
-    line = line.rstrip('\n') # 줄바꿈 제거
-    lower = 0 
+    line = line.rstrip('\n')
+    lower = 0
     upper = 0
-    digit = 0
+    num = 0
     space = 0
     for char in line:
         if char.islower():
-            lower +=1
+            lower += 1
         elif char.isupper():
-            upper +=1
+            upper += 1
         elif char.isdigit():
-            digit +=1
+            num += 1
         elif char == ' ':
-            space +=1
-    print(lower, upper, digit, space)
+            space += 1
+    print(lower, upper, num, space)
